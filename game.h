78f4d1fa2f56eb63bbe16a1e6681cb596375a74a
ZdziebkoAntonia <antoniazdz5@gmail.com>
2026-01-28 20:28:47 +0100

    gra

---
+++ game.h
@@ -0,0 +1,28 @@
+#ifndef GAME_H
+#define GAME_H
+
+#include <stdint.h> 
+
+#define MAX_WORD_LEN 32
+#define MAX_WRONG_GUESSES 7
+
+struct game {
+    int active;                 //czy jset ktos w grze i jest aktywna - 2 osoby
+    int player1_fd;             // gniazdo 1
+    int player2_fd;             // gniazdo 2
+
+    char word[MAX_WORD_LEN];    //haslo do zgadniecia
+    char guessed[MAX_WORD_LEN]; //zgadywane 
+    int wrong_guesse;          //liczba błędów
+};
+
+void game_init(void);
+void send_game(void);
+void game_reset(void);
+
+int start_game(int desc2);
+void guess(int desc2, char letter);
+int all_guessed(void);
+
+
+#endif
