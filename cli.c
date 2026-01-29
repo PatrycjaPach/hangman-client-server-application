d722da1b111cef243996ad5d570ccc2b3f11631b
ppach <patrycja.apch@onet.pl>
2026-01-29 18:11:34 +0100

    1

--- cli.c
+++ cli.c
@@ -18,14 +18,6 @@ int main(int argc, char **argv){
 	char buf[4096]; //do echa
 	ssize_t n; //do echa
 
-
-        //obsluga bledu jakby weszlo za duzo argumentow
-      /* if(argc !=2){
-                fprintf(stderr, "WARNING: invalid number of arguments %s\n", argv[0]);
-                return 1;
-        }*/ 
-
-	//SOCKET: obsluga bledu i stworzenie gniazda
 	if ((desc = socket(AF_INET6, SOCK_STREAM, 0)) < 0) {
 		perror("socket");
 		return 1;
@@ -36,28 +28,13 @@ int main(int argc, char **argv){
 	servaddr.sin6_family=AF_INET6;
 	servaddr.sin6_port=htons(1234);
 	
-	/*tlumaczenie adresu na porzadek sieciowy 
-	razem z obsluga bledu (do connect)*/
-	/*err_inet_pton=inet_pton(AF_INET6, argv[1], &servaddr.sin6_addr);
-	
-	if(err_inet_pton==0){
-		fprintf(stderr, "Invalid IPv6 addres\n");
-		return 1;
-	} 
-	if(err_inet_pton<0){
-		perror("inet_pton");
-		return 1;
-	}*/
     //zamiast tego co na gorze, do multicastu:
     if (discover_server(&servaddr) < 0) { //wywolanie funkcji do szukania servera przez multicast
         fprintf(stderr, "Nie znaleziono serwera\n");
         close(desc); //zamkniecie gniazda jak sie nie uda wszykuac servera albo connect zrobic, bo inaczej byloyby nie uzyteczne
         return 1;
     }
-    /*if (connect(desc, (struct sockaddr*)&servaddr, sizeof(servaddr)) < 0) {
-        perror("connect");
-        return 1;
-    }*/
+
     //zmiana connect pod multicast:
     if (connect(desc, (struct sockaddr*)&servaddr, sizeof(servaddr)) < 0) {
         perror("connect");
@@ -65,7 +42,7 @@ int main(int argc, char **argv){
         return 1;
     }
 	
-	//echo z chata
+
 	while (1) {
 		// 1) weź dane od użytkownika
 		n = read(STDIN_FILENO, buf, sizeof(buf)-1); //o
@@ -73,9 +50,7 @@ int main(int argc, char **argv){
 		if (n < 0) { perror("read stdin"); break; }//o
 		buf[n] = '\0';//o
 
-		// 2) wyślij do serwera
-		//if (write(desc, buf, n) < 0) { perror("write socket"); break; }
-
+		
 		//zamiast write, pod tlv:
 		if (strncmp(buf, "LOGIN ", 6) == 0) {
 			char *username = buf + 6;
@@ -94,10 +69,7 @@ int main(int argc, char **argv){
 			printf("Unknown command\n");
 			continue;
 		}
-		// 3) odbierz echo z serwera
-		//n = read(desc, buf, sizeof(buf));
-		//if (n == 0) break; // serwer zamknął połączenie
-		//if (n < 0) { perror("read socket"); break; } 
+
 
 		//zamaist read pod tlv: (pełni podobne funkcje)
 		uint16_t type;
@@ -116,10 +88,9 @@ int main(int argc, char **argv){
 			write(STDOUT_FILENO, rbuf, rlen);
 		}
 
-		// 4) wypisz na ekran
-		//if (write(STDOUT_FILENO, buf, n) < 0) { perror("write stdout"); break; }
+
 	}
-	//koniec echa z chata
+
 
     close(desc); 
     return 0;
